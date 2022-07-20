9927e071c44c12af2add09e23e8bae0caa84dfa5
shariarmozumder <mdshariar00@gmail.com>
2022-07-20 19:38:21 +0600

    typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Counter from "./Components/Counter";
+import Users from "./Components/Users";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter></Counter>
+      <Users></Users>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/User.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+import UserModel from "../types/models";
+
+
+interface Props {
+  user: UserModel;
+  addUser: (user: UserModel) => void;
+}
+const User: FC<Props> = ({ user, addUser }) => {
+  return (
+    <div>
+      <h2>
+        Hello from : {user.name} with experience {user.email}
+      </h2>
+      <button onClick={() => addUser(user)}>add me</button>
+    </div>
+  );
+};
+
+export default User;
